f6ca3f6b5e1d63438c48b21bfce45512781859d6
RangerDevv <66392962+RangerDevv@users.noreply.github.com>
2022-02-23 19:59:08 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+const helo = document.getElementById('hello');
+
+function change () {
+  helo.innerHTML = 'I am Rang3r!';
+}
+
+setInterval(change, 3000);
+
+function change2 () {
+  helo.innerHTML = 'Oh hi! i am just doing stuff here!';
+}
+
+setInterval(change2, 3000);
